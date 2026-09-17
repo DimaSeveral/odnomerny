9f4eb373a2cf78c8af0098f0502449609395d9d1
Kawaakari <pachik2018@icloud.com>
2026-09-17 14:27:29 +0700

    Последний код
    Код, в котором нет минимальной температуры, программа ориентируется только на максимальное количество итераций. Так же присутствует внешний цикл, для того, чтоб было удобнее проводить эксперимент и находить среднее значение функции.

--- Function.cpp
+++ Function.cpp
@@ -2,144 +2,133 @@
 #include <math.h>
 #include <random>
 #include <time.h>
-#include <windows.h>
 
 using namespace std;
 
-double Foo(double x) // Начальная функция
+double Foo(const double* x)
 {
-    double exp_term = exp(-2.77257 * x * x);
-    double part1 = 0.05 * pow(x - 1, 2);
-    double part2 = (3 - 2.9 * exp_term) * (1 - cos(x * (4 - 50 * exp_term)));
-    return part1 + part2;
+    double x1 = x[0];
+    double x2 = x[1];
+    double part1 = -1 + pow((sin(sqrt(x1*x1 + x2*x2))), 2);
+    double part2 = 1 + 0.001 * (x1*x1 + x2*x2);
+    double result = part1 / part2;
+    return result;
 }
 
-// Функция генерации нормально распределенного числа (ЦПТ)
-/*` NormalRandom_CPT(double mu, double sigma)
-{
-    double sum = 0;
-    for (int i = 0; i < 12; i++)
-    {
-        sum += (double)rand() / RAND_MAX;
-    }
-    double z = sum - 6.0;
-    return mu + sigma * z;
-}
-*/
 int main()
 {
     setlocale(LC_ALL, "");
-    srand((unsigned)time(NULL));
 
-    double D = 0.01;
-    double sigma = sqrt(D);
-    double F = 0.95445161895;
-    double xmin = -1;
-    double xmax = 1;
+    double minXY = -10.0;
+    double maxXY = 10.0;
+    int n = 2;
 
-    double xc = (double)rand() / RAND_MAX * (xmax - xmin) + xmin;
-    double fxc = Foo(xc);
+    double T_start = 1000.0;
+    double alpha = 0.998;
+    double T_min = 0.0000001;
+    int maxIterations = 4000;
+    int numRuns = 100; // Количество запусков внешнего цикла
 
-    // Переменные для запоминания лучшего результата за всё время
-    double best_xc = xc;
-    double best_fxc = fxc;
-
-    cout << "Начальная точка xc: " << xc << ", f(xc) = " << fxc << endl;
-    cout << "Целевая точка F: " << F << ", f(F) = " << Foo(F) << endl;
-
-    double T = 1000.0;       // Начальная "температура" (должна быть сопоставима с диапазоном значений функции)
-    double alpha = 0.95;  // Коэффициент охлаждения (чем ближе к 1, тем медленнее остывает)
-    double T_min = 0.0000001; // Минимальная температура, при которой алгоритм остановится
-    double xn;
-
-    // 1. Генерируем новую точку
+    // Генератор случайных чисел
     random_device rd;
     mt19937 gen(rd());
+    uniform_real_distribution<double> uniform_rl(minXY, maxXY);
+    normal_distribution<double> distribution(0.0, 1.0);
+    uniform_real_distribution<double> prob_dist(0.0, 0.8);
 
+    //  массив для хранения истории лучших значений
+    double* history_BestValues = new double[numRuns];
 
-    normal_distribution<double> distribution(0.0, 0.3);
-
-    int maxIterations = 2000;
-
-
-
-    for (int iter = 1; iter <= maxIterations; iter++)
+    // внешний цикл (10 итераций)
+    for (int run = 1; run <= numRuns; ++run)
     {
+        double T = T_start; // Сброс температуры для каждого запуска
 
-        xn = xc + distribution(gen);
+        double* curr = new double[n];
+        double* next = new double[n];
+        double* best = new double[n];
 
-        if (xn < -1)
+        // Начальная точка
+        for (int i = 0; i < n; ++i)
         {
-            xn = -1;
+            curr[i] = uniform_rl(gen);
+            best[i] = curr[i];
         }
-        else if (xn > 1)
+
+        double Value_Curr = Foo(curr);
+        double Value_Best = Value_Curr;
+
+        // Внутренний цикл имитации отжига
+        for (int iter = 1; iter <= maxIterations; iter++)
         {
-            xn = 1;
-        }
+            for (int i = 0; i < n; ++i)
+            {
+                next[i] = curr[i] + distribution(gen);
+                if (next[i] < minXY) next[i] = minXY;
+                else if (next[i] > maxXY) next[i] = maxXY;
+            }
 
+            double Next_Value = Foo(next);
+            double delta = Next_Value - Value_Curr;
 
-        double fxn = Foo(xn);
-        double delta = fxn - fxc; // Разница между новым и текущим значением
-
-        // 2. Логика принятия решения (Ядро имитации отжига)
-        bool accept = false;
-
-        if (delta < 0)
-        {
-            // Если стало ЛУЧШЕ (функция уменьшилась) - принимаем всегда
-            accept = true;
-        }
-        else
-        {
-            // Если стало ХУЖЕ - принимаем с вероятностью, зависящей от температуры
-            double probability = exp(-delta / T); // Формула отжига
-            double r = (double)rand() / double(RAND_MAX); // Случайное число от 0 до 1
-
-            if (r < probability)
+            bool accept = false;
+            if (delta < 0)
             {
                 accept = true;
             }
-        }
-
-        // 3. Если решили принять новую точку
-        if (accept)
-        {
-            xc = xn;
-            fxc = fxn;
-
-            // Обновляем абсолютный рекорд, если текущая точка стала лучше всех предыдущих
-            if (fxc < best_fxc)
+            else
             {
-                best_xc = xc;
-                best_fxc = fxc;
+                double probability = exp(-delta / T);
+                if (prob_dist(gen) < probability)
+                {
+                    accept = true;
+                }
             }
+
+            if (accept)
+            {
+                for (int i = 0; i < n; ++i) curr[i] = next[i];
+                Value_Curr = Next_Value;
+
+                // Исправленная логика обновления абсолютного рекорда
+                if (Value_Curr < Value_Best)
+                {
+                    for (int i = 0; i < n; ++i) best[i] = curr[i];
+                    Value_Best = Value_Curr;
+                }
+            }
+
+            T = T * alpha;
+
+            //if (T < T_min) break;
         }
 
-        // 4. Охлаждение системы
-        T = T * alpha;
+        cout << "Лучшее значение: f = " << Value_Best
+             <<  endl;
 
-        // Вывод прогресса каждые 10 итераций
-        if (iter % 2 == 0)
-        {
-            cout << "Итерация " << iter
-                 << "  Температура: " << T
-                 << "  Текущая f(xc): " << fxc
-                 << "  ЛУЧШАЯ f(x): " << best_fxc
-                 << " xc = " << best_xc << endl;
-        }
+        // Индекс run - 1, так как нумерация массивов начинается с 0
+        history_BestValues[run - 1] = Value_Best;
 
-        // Если температура упала ниже минимума, дальнейший поиск бессмысленен
-        if (T < T_min)
-        {
-            cout << "\n[Досрочная остановка: достигнута минимальная температура]" << endl;
-            break;
-        }
+        // Освобождаем память внутреннего цикла
+        delete[] curr;
+        delete[] next;
+        delete[] best;
+    };
+
+    double summa = 0.0;
+
+    cout << "История лучших значений по запускам:" << endl;
+    for (int i = 0; i < numRuns; ++i)
+    {
+        summa += history_BestValues[i]; // Накапливаем сумму
     }
 
-    cout << "Результат:" << endl;
-    cout << "Лучшая найденная точка: xc = " << best_xc << endl;
-    cout << "Значение функции в ней: f(xc) = " << best_fxc << endl;
-    cout << "Для сравнения, f(F) = " << Foo(F) << endl;
+    double average = summa / numRuns;
+
+    cout << "\nСумма всех лучших значений (summa) : " << summa << endl;
+    cout << "Среднее значение (average)         : " << average << endl;
+
+    delete[] history_BestValues;
 
     return 0;
 }
